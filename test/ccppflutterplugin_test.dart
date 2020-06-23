fcc2b3e237e0594df5189bfbdb6b1146cb343da8
Atit Sornara <sornara.work@gmail.com>
2020-06-23 19:50:06 +0700

    Remove unused test case

--- test/ccppflutterplugin_test.dart
+++ test/ccppflutterplugin_test.dart
@@ -3,21 +3,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ccppflutterplugin/ccppflutterplugin.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('ccppflutterplugin');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
+//  const MethodChannel channel = MethodChannel('ccppflutterplugin');
+//
+//  TestWidgetsFlutterBinding.ensureInitialized();
+//
+//  setUp(() {
+//    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+//      return '42';
+//    });
+//  });
+//
+//  tearDown(() {
+//    channel.setMockMethodCallHandler(null);
+//  });
+//
+//  test('getPlatformVersion', () async {
 //    expect(await CcppFlutterPlugin.platformVersion, '42');
-  });
+//  });
 }
