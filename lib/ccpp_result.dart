950c9ca12883b7b2e75def8d73ed69d4c20eaaa6
Narut <cpe.toniz@gmail.com>
2021-07-16 18:07:55 +0700

    fix: object

--- lib/ccpp_result.dart
+++ lib/ccpp_result.dart
@@ -11,8 +11,8 @@ class CcppResult {
 
   factory CcppResult.fromJson(Map<String, dynamic> json) {
     return CcppResult(
-      invoiceNo: json['invoiceNo'] as String,
-      errorMessage: json['errorMessage'] as String,
+      invoiceNo: json['invoiceNo'] as String?,
+      errorMessage: json['errorMessage'] as String?,
     );
   }
 }
